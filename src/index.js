6c5726e4fcab212e314f49a2bd34307564824472
Youcef Boutrig <78567400+Herelius@users.noreply.github.com>
2021-06-22 10:35:08 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+reducer(0, { type: "INCREMENT" });
+// Will return 1
